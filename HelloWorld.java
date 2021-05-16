83e8edfb8ec36d99548c2178c7da5ea4ac61aeef
vikash011 <70995603+vikash011@users.noreply.github.com>
2021-05-16 13:59:19 +0530

    Tracing log message added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
 
      public static void main(String[] args){
+          System.out.println("HelloWorld::main method");
          System.out.println("HelloWorld");
    }
 
